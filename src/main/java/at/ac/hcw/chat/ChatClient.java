0c604b4686adb2b3ae44524f62643d6a601fb153
Ghazal ArbabzadehBoroujeni <ghazalab96@gmail.com>
2025-11-19 17:20:08 +0100

    second version of cahat_tool

---
+++ src/main/java/at/ac/hcw/chat/ChatClient.java
@@ -0,0 +1,110 @@
+package at.ac.hcw.chat;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+// for reading data from streams like file, socket or console
+// what we use: InputStreamReader, BufferedReader, PrintWriter (auto flush = true)
+// => necessary for sending and receiving the text messages
+import java.net.*;
+import java.net.Socket;
+import java.util.Scanner;
+// classes for network communication
+// ServerSocket for opening a port and listening until client is connected
+// Socket for establishing a TCP connection between two computers
+
+
+public class ChatClient {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        // Host and port to connect to
+        System.out.print("Enter ip address: ");
+        String host=scanner.nextLine();// since both on same machine
+        System.out.print("Enter port: ");
+        int port=scanner.nextInt();
+        // client connects to the server through the socket (IP and port)
+        try (Socket socket = new Socket(host, port)){
+            System.out.println("Connected to server successfully!");
+
+            // Input stream to receive messages from server
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+
+            // Output stream to send text to server
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(),
+                    true
+            ); // auto-flush enabled
+
+            // Reads user input from console
+            BufferedReader userInput = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+
+            // Thread responsible for receiving messages from the server
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    String received;
+                    while ((received = in.readLine()) != null) {
+
+                        // If server sends "exit", close connection
+                        if (received.equalsIgnoreCase("exit")) {
+                            System.out.println("Server requested disconnect");
+                            out.println("exit");
+                            break;
+                        }
+                        System.out.println("Server: " + received);
+                    }
+                } catch (IOException e) {
+                    System.out.println("Connection closed unexpectedly");
+                }
+            });
+
+            // Thread responsible for sending messages to the server
+            Thread sendThread = new Thread(() -> {
+                try {
+                    String message;
+                    while ((message = userInput.readLine()) != null) {
+
+                        out.println(message); // send message to server
+                        System.out.println("Message delivered.");
+
+                        // If client types "exit", close connection
+                        if (message.equalsIgnoreCase("exit")) {
+                            System.out.println("Client requested disconnect");
+                            // Notify server
+                            out.println("exit");
+
+                            socket.shutdownOutput();
+                            socket.shutdownInput();
+
+                            break; //exit the loop and end the thread
+                        }
+                    }
+                    //after exiting the loop, close the socket so receiveThread sees null.
+
+                } catch (IOException e) {
+                    System.err.println("Error while client sending message: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            });
+
+            // Start both threads
+            receiveThread.start();
+            sendThread.start();
+
+            // Wait until both threads finish before ending the program
+            receiveThread.join();
+            sendThread.join();
+
+            System.out.println("Client stopped safely");
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Client error: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/at/ac/hcw/chat/ChatServer.java
@@ -0,0 +1,155 @@
+package at.ac.hcw.chat;
+
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+// for reading data from streams like file, socket or console
+// what we use: InputStreamReader, BufferedReader, PrintWriter (auto flush = true)
+// => necessary for sending and receiving the text messages
+
+import java.net.ServerSocket;
+import java.net.Socket;
+// classes for network communication
+// ServerSocket for opening a port and listening until client is connected
+// Socket for establishing a TCP connection between two computers
+
+
+// defining the main class called ChatServer
+public class ChatServer {
+    public static void main(String[] args) throws Exception {
+        //the port where server will listen for connections
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Port : ");
+        int port = scanner.nextInt();// port input
+        String ip = getLocalIPv4();
+        System.out.println("Ip is : " + ip);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
+
+            //wait until a client connects, this is a blocking call
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected successfully!");
+
+            //These streams allow the server to send and receive text messages
+            InputStreamReader inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+            BufferedReader in = new BufferedReader(inputStreamReader);
+
+            // better option than BuffereWriter because of: 1- println 2-autoFlush 3-speed is not important here cause the messages are short.
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(),
+                    true
+            ); // auto-flush enabled
+
+            // Read user input from server console
+            BufferedReader userInput = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+
+            //Thread responsible for receiving messages from the client
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    String received;
+                    while ((received = in.readLine()) != null) {
+                        //if client sends "exit", stop hte connection
+                        if (received.equalsIgnoreCase("exit")) {
+                            System.out.println("Client requested disconnect");
+                            out.println("exit");
+                            break;
+                        }
+                        System.out.println("Client: " + received);
+                    }
+                } catch (IOException e) {
+                    // socket closed is expected when exit is called
+                    System.out.println("Connection closed by server or client.");
+                    ;
+                }
+            });
+
+            // Thread responsible for sending messages from server user to the client
+            Thread sendThread = new Thread(() -> {
+                try {
+                    String message;
+                    while ((message = userInput.readLine()) != null) {
+                        out.println(message); // send message to the client
+                        System.out.println("Message delivered.");
+                        //if server types exit, close connection
+                        if (message.equalsIgnoreCase("exit")) {
+                            System.out.println("Server requested disconnect");
+                            // Notify client
+                            out.println("exit");
+
+                            clientSocket.shutdownOutput();
+                            clientSocket.shutdownInput();
+                            break; //exit the loop and end the thread
+                        }
+                    }
+                    //after exiting the loop, close the socket so receiveThread sees null.
+
+                } catch (IOException e) {
+                    System.err.println("Error happened while server was communicating with the client: " + e.getMessage());
+                    e.printStackTrace();   // optional for debugging
+                }
+            });
+
+            //start both threads simultaneously
+            receiveThread.start();
+            sendThread.start();
+
+            //wait until both threads finish before closing socket
+            try {
+                receiveThread.join();
+                sendThread.join();
+            } catch (InterruptedException e) {
+                System.err.println("Thread interrupted: " + e.getMessage());
+                Thread.currentThread().interrupt(); // preserve interrupt status
+            }
+
+
+            // guaranties the safe port kill (so we don't get the "Already in use" message.
+            //close client
+            clientSocket.close();
+            System.out.println("Client disconnected");
+            //close server
+            serverSocket.close();
+            System.out.println("Server stopped safely");
+
+        } catch (IOException e) {
+            System.out.println("Server error: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    // Radi
+
+    public static String getLocalIPv4() throws SocketException { // get ipv4 address function
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+
+        while (nets.hasMoreElements()) {
+            NetworkInterface nif = nets.nextElement();
+
+            if (!nif.isUp() || nif.isLoopback() || nif.isVirtual())
+                continue;
+
+            for (InterfaceAddress ia : nif.getInterfaceAddresses()) {
+                InetAddress addr = ia.getAddress();
+
+                if (addr instanceof Inet4Address) {
+                    String ip = addr.getHostAddress();
+
+                    // nur private lokale IPv4-Adressen zurückgeben
+                    if (isPrivateIPv4(ip)) {
+                        return ip;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private static boolean isPrivateIPv4(String ip) { // to check if its a private ip address or just loopback
+        return ip.startsWith("10.") ||
+                ip.startsWith("192.168.") ||
+                ip.matches("^172\\.(1[6-9]|2[0-9]|3[0-1])\\..*");
+    }
+}
